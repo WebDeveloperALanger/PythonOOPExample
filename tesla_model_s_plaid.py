18b1175390657f5669364d0e1b9662ff3cac39a0
Andre <aldev@langer.guru>
2022-08-28 19:56:30 +0200

    Tesla Model S Plaid added

---
+++ tesla_model_s_plaid.py
@@ -0,0 +1,45 @@
+class Car:
+    def __init__(self, manufacturer, model_name, max_speed):
+        self.manufacturer = manufacturer
+        self.model_name = model_name
+        self.max_speed = max_speed
+        self.current_speed = 0
+
+    def increase_speed(self, kmh):
+        new_speed = self.current_speed + kmh
+        if new_speed > self.max_speed:
+            self.current_speed = self.max_speed
+        else:
+            self.current_speed = new_speed
+
+    def reduce_speed(self, kmh):
+        new_speed = self.current_speed - kmh
+        if new_speed <= 0:
+            self.current_speed = 0
+        else:
+            self.current_speed = new_speed
+
+    def info(self):
+        return self.manufacturer + " " + self.model_name + " at " + str(self.current_speed) + " km/h"
+
+    def print_info(self):
+        print(self.info())
+
+
+class TeslaModelSPlaid(Car):
+    def __init__(self):
+        super().__init__("Tesla", "Model S Plaid", 322)
+
+
+MyNewCar = TeslaModelSPlaid()
+
+MyNewCar.increase_speed(50)
+MyNewCar.increase_speed(50)
+MyNewCar.increase_speed(50)
+MyNewCar.print_info()           # Tesla Model S Plaid at 150 km/h
+
+MyNewCar.increase_speed(800)
+MyNewCar.print_info()           # Tesla Model S Plaid at 322 km/h
+
+MyNewCar.reduce_speed(400)
+MyNewCar.print_info()           # Tesla Model S Plaid at 0 km/h
